a64dc28c849232ea1d3780ccfcff4644cfa56bbd
ChristiaanWewer <christiaanwewer@hotmail.com>
2018-01-10 16:03:12 +0100

    added project folder

---
+++ Cryptonite/app/src/main/java/com/dev/chris/cryptonite/CryptoAdapter.java
@@ -0,0 +1,52 @@
+package com.dev.chris.cryptonite;
+
+import android.content.Context;
+
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+/**
+ * Created by chris on 3-1-2018.
+ */
+
+public class CryptoAdapter extends ArrayAdapter {
+    private Context context;
+    private ArrayList<CryptoCoinData> cryptoCoinData;
+
+    public CryptoAdapter(Context context, ArrayList<CryptoCoinData> cryptoCoinData) {
+        super(context, R.layout.cryptocoin_row, cryptoCoinData);
+
+        this.cryptoCoinData = cryptoCoinData;
+        this.context = context;
+        Log.d("check if exists", cryptoCoinData.get(0).getCoinName());
+
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        LayoutInflater layoutInflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        View view = layoutInflater.inflate(R.layout.cryptocoin_row, parent, false);
+
+        TextView numberRowTextView = view.findViewById(R.id.numberRowTextView);
+        TextView nameRowTextView = view.findViewById(R.id.nameRowTextView);
+        TextView priceRowTextView = view.findViewById(R.id.priceRowTextView);
+        TextView priceChangeRowTextView = view.findViewById(R.id.priceChangeRowTextView);
+
+        numberRowTextView.setText(String.valueOf(cryptoCoinData.get(position).getRank()));
+        nameRowTextView.setText(cryptoCoinData.get(position).getCoinName());
+        priceRowTextView.setText(Double.toString(cryptoCoinData.get(position).getPriceUsd()));
+        priceChangeRowTextView.setText(Double.toString(cryptoCoinData.get(position).getChange24h()));
+
+        Log.d("adapter", "adapter is getting used");
+
+        return view;
+    }
+
+}
---
+++ Cryptonite/app/src/main/java/com/dev/chris/cryptonite/CryptoniteActivity.java
@@ -0,0 +1,40 @@
+package com.dev.chris.cryptonite;
+
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+
+public class CryptoniteActivity extends AppCompatActivity {
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cryptonite);
+
+        Log.d("oncreate", "it works");
+        FragmentManager fm = getSupportFragmentManager();
+        CryptoFragment cryptoFragment = new CryptoFragment();
+        Log.d("fragment", cryptoFragment.toString());
+        FragmentTransaction ft = fm.beginTransaction();
+        ft.replace(R.id.crypto_fragment_container, cryptoFragment);
+        ft.commit();
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.actions, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+       Log.d("works", "works");
+       return super.onOptionsItemSelected(item);
+    }
+}
